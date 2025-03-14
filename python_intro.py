b0517e47966efecafb476f0d1f0fe16f532c6d3b
cogni <rajgy32@gmail.com>
2025-03-14 16:42:38 +0545

    FIRST INITIATION

---
+++ python_intro.py
@@ -0,0 +1,30 @@
+# Mult-paradigmmprogramming language
+# Interpreted language
+# Supports dynamic data type
+# Independent from platforms
+# Focused on development time
+# Simple and easy grammar
+# High-level internal object data types
+# Automic memory management
+# Its free (open source)!
+#
+#
+# =======HISTORY====
+# Python born, name picked - Dec 1989
+# By Guido van Rossum, not at Dropbox
+# First public release(USENET)- fEB 1991
+# python.org website - 1996 0r 1997
+#
+# ====WHY PYTHON====
+# Fun to use "Scripting-language"
+# Object-Oriented, imperative, functional programming and procedural styles
+# highly educational
+#
+# ===========WHERE TO USE====
+# FOR web development
+# data science, including machine learning, data analysis, and data visualization
+# sctipying
+# GUI, GAMING
+#
+# ======WHICH VERSION=====
+# PYTHON 3 IS MOST SUITABLE
